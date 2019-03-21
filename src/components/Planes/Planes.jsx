e3430a86a7dd466f741d0ef68fc0bab5f7b6766e
kookydev <info@kooky.dev>
2019-03-21 16:02:09 +0000

    Planes Component setup

---
+++ src/components/Planes/Planes.jsx
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+
+class Planes extends Component {
+  state = { track: 0, callsign: "EZY185", height: 6500 };
+  render() {
+    return <div>hello</div>;
+  }
+}
+
+export default Planes;
